d05c7d8bd11d2b737a91e933a4543107f7f34f95
Ervin543 <ervindasratt@gmail.com>
2023-05-03 22:24:49 -0500

    completed TODOs in the webpack.config.js file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,76 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-
 const { GenerateSW } = require('workbox-webpack-plugin');
-
-// ...
-
-plugins: [
-  new HtmlWebpackPlugin({
-    template: './src/index.html',
-    filename: 'index.html',
-    chunks: ['main'],
-  }),
-  new HtmlWebpackPlugin({
-    template: './src/install.html',
-    filename: 'install.html',
-    chunks: ['install'],
-  }),
-  new WebpackPwaManifest({
-    name: 'My Progressive Web App',
-    short_name: 'MyPWA',
-    start_url: '/',
-    display: 'standalone',
-    icons: [
-      {
-        src: path.resolve('src/assets/icon.png'),
-        sizes: [96, 128, 192, 256, 384, 512],
-        type: 'image/png',
-        purpose: 'any maskable',
-      },
-    ],
-  }),
-  new GenerateSW({
-    swDest: 'sw.js',
-    clientsClaim: true,
-    skipWaiting: true,
-  }),
-  new InjectManifest({
-    swSrc: './src/sw.js',
-    swDest: 'sw.js',
-  }),
-],
-
-// ...
-
-
-// TODO: Add CSS loaders and babel to webpack.
-
-
-module: {
-  rules: [
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    },
-    {
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env'],
-        },
-      },
-    },
-  ],
-},
-
-
-
 
 module.exports = () => {
   return {
@@ -85,12 +16,56 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        chunks: ['main'],
+      }),
+      new HtmlWebpackPlugin({
+        template: './src/install.html',
+        filename: 'install.html',
+        chunks: ['install'],
+      }),
+      new WebpackPwaManifest({
+        name: 'My Progressive Web App',
+        short_name: 'MyPWA',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      }),
+      new GenerateSW({
+        swDest: 'sw.js',
+        clientsClaim: true,
+        skipWaiting: true,
+      }),
+      new InjectManifest({
+        swSrc: './src/sw.js',
+        swDest: 'sw.js',
+      }),
     ],
-
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
